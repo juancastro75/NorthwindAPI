e71685a346504a48c9e866f7dfdbf2e21ec91849
Juan Castro <juancho33452@gmail.com>
2021-04-25 21:54:04 -0500

    Tarea 3 - Crear una API con tres controladores

---
+++ DTOs/EmployeeDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NorthwindAPI.DTOs
+{
+    public class EmployeeDTO
+    {
+        public EmployeeDTO()
+        {
+
+        }
+
+        public int id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Pais { get; set; }
+        public string Ciudad { get; set; }
+        public string Direccion { get; set; }
+        public DateTime? Nacimiento { get; set; }
+    }
+}
+
---
+++ DTOs/OrderDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NorthwindAPI.DTOs
+{
+    public class OrderDTO
+    {
+        public OrderDTO()
+        {
+
+        }
+
+        public int id { get; set; }
+        public decimal? Peso { get; set; }
+        public string Direccion { get; set; }
+        public string EnviadoPor { get; set; }
+        public DateTime? FechaDePedido { get; set; }
+        public DateTime? FechaRequerida { get; set; }
+        public DateTime? FechaDeEnvio { get; set; }
+    }
+}
+
